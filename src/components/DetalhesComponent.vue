ebdc423732f863d9f18d9656b8efd9c73fb2c800
rodolfo-acti <53875025+rodolfinhoc@users.noreply.github.com>
2023-05-19 14:07:45 -0300

    Update DetalhesComponent.vue

--- src/components/DetalhesComponent.vue
+++ src/components/DetalhesComponent.vue
@@ -1,8 +1,18 @@
 <template>
+  <div class="card md:flex md:justify-content-center">
+      <Galleria :value="images" :responsiveOptions="responsiveOptions" :numVisible="5" containerStyle="max-width: 640px">
+          <template #item="slotProps">
+              <img :src="images[0].back_default" :alt="slotProps.item.alt" style="width: 100%" />
+          </template>
+          <template #thumbnail="slotProps">
+              <img :src="slotProps.item.thumbnailImageSrc" :alt="slotProps.item.alt" />
+          </template>
+      </Galleria>
+  </div>
 </template>
   
   <script lang="ts">
-  import { defineComponent, inject, toRaw } from 'vue';
+  import { defineComponent, inject, ref, toRaw } from 'vue';
   
   export default defineComponent({
     name: 'DetalhesComponent',
@@ -11,9 +21,27 @@
     setup(){
       const dialogRef = inject('dialogRef') as any;
       const pokemonDetalhes = toRaw(dialogRef.value.data);
+      const images = ref<any[]>([]);
+      images.value.push(pokemonDetalhes.sprites);
+      const responsiveOptions = ref([
+          {
+              breakpoint: '991px',
+              numVisible: 4
+          },
+          {
+              breakpoint: '767px',
+              numVisible: 3
+          },
+          {
+              breakpoint: '575px',
+              numVisible: 1
+          }
+      ]);
       console.log(pokemonDetalhes);
       return {
-        pokemonDetalhes
+        pokemonDetalhes,
+        responsiveOptions,
+        images
       }
     },
   });
